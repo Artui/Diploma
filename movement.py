22313466bce233d07c7bad6d4007d528549f2138
arturveres <artur8118@gmail.com>
2018-06-04 18:26:53 +0300

    Fixing baudrate

--- movement.py
+++ movement.py
@@ -1,36 +1,44 @@
-import serial
 import time
+
+import serial
 
 ser = None
 
+
 def get_connection():
-	ser = serial.Serial()
-	ser.port = "/dev/ttyUSB0"
-	ser.baudrate = 155200
-	ser.open()
-	return ser
+    global ser
+    ser = serial.Serial()
+    ser.port = "/dev/ttyUSB0"
+    ser.baudrate = 115200
+    ser.open()
+    return ser
+
 
 def close_connection():
-	ser.close()
+    ser.close()
 
 
 def start_wheel(index):
-	if ser and ser.is_open:
-		for i in ["M", str(index), "+", "1", "0", "0"]:
-			time.sleep(0.5)
-			ser.write(bytes(i))
+    global ser
+    if ser:
+        for i in ["M", str(index), "+", "0", "5", "0"]:
+            time.sleep(0.1)
+            ser.write(bytes(i))
+
 
 def start_all_wheels():
-	for i in range(1, 5):
-		start_wheel(i)
+    for i in range(1, 5):
+        start_wheel(i)
+
 
 def stop_wheel(index):
-	if ser and ser.is_open:
-		for i in ["M", str(index), "+", "0", "0", "0"]:
-			time.sleep(0.5)
-			ser.write(bytes(i))
+    global ser
+    if ser:
+        for i in ["M", str(index), "+", "0", "0", "0"]:
+            time.sleep(0.1)
+            ser.write(bytes(i))
+
 
 def stop_all_wheels():
-	for i in range(1, 5):
-		stop_wheel(i)
-			
+    for i in range(1, 5):
+        stop_wheel(i)
