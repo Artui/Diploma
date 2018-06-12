e09111e9e7b6b094620b9237655477b6ef5308ad
arturveres <artur8118@gmail.com>
2018-06-12 18:27:37 +0300

    Instructions

--- distance.py
+++ distance.py
@@ -22,10 +22,11 @@ def get_distance_lists():
     data_int = [ord(val) for val in data]
     glob_temp, temp = [], []
     for i in data_int:
-        if i == 36:
-            if temp:
-                glob_temp.append(temp)
+        if i == 36 and len(temp) == 0:
             temp = [i]
         else:
             temp.append(i)
+            if len(temp) == 8:
+                glob_temp.append(temp)
+                temp = []
     return glob_temp[0] if glob_temp else None
