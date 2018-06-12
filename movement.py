d323266337782a36f8c6d936a3ddd19aa8a51dbd
arturveres <artur8118@gmail.com>
2018-06-12 19:53:48 +0300

    Instructions

--- movement.py
+++ movement.py
@@ -40,17 +40,17 @@ class Movement:
 
     def turn_right(self, timeout):
         self.start_wheel(2, '100')
-        self.start_wheel_back(1, '100')
+        self.start_wheel_back(3, '100')
         time.sleep(timeout)
-        self.stop_wheel(1)
         self.stop_wheel(2)
+        self.stop_wheel(3)
 
     def turn_left(self, timeout):
         self.start_wheel(1, '100')
-        self.start_wheel_back(2, '100')
+        self.start_wheel_back(4, '100')
         time.sleep(timeout)
         self.stop_wheel(1)
-        self.stop_wheel(2)
+        self.stop_wheel(4)
 
     def start_all_wheels(self, speed):
         for i in range(1, 5):
